e44d0607c36c5b37155dd38f63a36fcbb8bf4fb7
devjanaprime <dev@primeacademy.io>
2016-12-02 15:01:59 -0600

    first

---
+++ readme.md
@@ -0,0 +1,22 @@
+JavaScript and JQuery Review
+============================
+
+JavaScript and JQuery review activity with an ajax call.
+
+Tech:
+-----
+* HTML
+* JS
+* JQuery
+
+We'll be hitting archive.org via a JQuery ajax call through the following url: https://archive.org/advancedsearch.php?q=SEARCHTEXT&output=json
+
+Steps:
+------
+* basic project setup (HTML, JS, JQ)
+* input field and button to capture user search text
+* assemble search url
+* ajax call to archive.org
+* console log out returned data
+* ID info needed within the returned info
+* display data from search to DOM
---
+++ scripts/app.js
@@ -0,0 +1,31 @@
+$( document ).ready( function(){
+  $( '#searchButton' ).on( 'click', function(){
+    // get user input from text input field with id 'searchIn'
+    var searchText = $( '#searchIn' ).val();
+    console.log( 'in searchButton on click:', searchText );
+    // assemble search url
+    var searchUrl = 'https://archive.org/advancedsearch.php?q=' + searchText + '&output=json';
+    // ajax call to archive.org
+    $.ajax({
+      url: searchUrl,
+      dataType: 'JSON',
+      success: function( data ){
+        // console log out returned data
+        console.log( 'in ajax success, data:' , data );
+        // ID info needed within the returned info
+        // after looking at the data returned we can see our info is in data.response.docs
+        // display data from search to DOM
+        showResults( data.response.docs );
+      }
+    }); // end ajax
+  }); //end searchButton on click
+  var showResults = function( searchResults ){
+    console.log( 'in showResults:', searchResults );
+    // loop through results and display on DOM
+    var outputText = '';
+    for (var i = 0; i < searchResults.length; i++) {
+      outputText += '<p><strong><a href="https://archive.org/details/' + searchResults[i].identifier + '">' + searchResults[i].identifier + '</a>:</strong> ' + searchResults[i].description + '</p>';
+    } // end for
+    $( '#outputDiv' ).html( outputText );
+  }; //end showResults
+}); // end doc ready
